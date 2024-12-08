84d1d7910f1e49562b533831d526b82e2a4e5e3a
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-08 21:05:51 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/configuration.nix
+++ src/configuration.nix
@@ -1,11 +1,12 @@
 { config, lib, pkgs, ... }: {
-  let repo = builtins.fetchGit {
-    url = "https://github.com/jaid/nix";
-  };
-  imports = [
-    ./hardware-configuration.nix
-    "${repo}/src/jaid.nix"
-  ];
+  imports =
+    let repo = builtins.fetchGit {
+      url = "https://github.com/jaid/nix";
+      ref = "main";
+    }; in [
+      ./hardware-configuration.nix
+      "${repo}/src/jaid.nix"
+    ];
   boot = {
     loader = {
       systemd-boot.enable = true;
@@ -15,4 +16,7 @@
     kernelPackages = pkgs.linuxPackages_latest;
   };
   system.stateVersion = "24.11";
+  environment.systemPackages = with pkgs; [
+    git
+  ];
 }
--- src/jaid.nix
+++ src/jaid.nix
@@ -7,5 +7,5 @@
     ];
     initialHashedPassword = "$6$bk5SDekXN9EAk3T/$IGwHtdQk/AVkH3Fdw48KIiGT4nr.xLvgTle7GibTuQHQGGWgRij5nU/RGMlo3..c9dqCjXLRg/i4JJRM9GcKy1";
     shell = pkgs.powershell;
-  }
+  };
 }
