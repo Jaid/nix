ce8641c94bb93e144f51e87d1fb592998e078e8a
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-08 21:15:45 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/jaid.nix
+++ src/jaid.nix
@@ -6,6 +6,9 @@
       "docker"
     ];
     initialHashedPassword = "$6$bk5SDekXN9EAk3T/$IGwHtdQk/AVkH3Fdw48KIiGT4nr.xLvgTle7GibTuQHQGGWgRij5nU/RGMlo3..c9dqCjXLRg/i4JJRM9GcKy1";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0Up24BhYxyHEWrYc5EJ5PbPn7hVYGpv1fSCwLURvGq jaid@github/73158609"
+    ];
     shell = pkgs.powershell;
   };
 }
