6cf75f3b27e19d2dc6e93fe26e8284bfdba9dbc8
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-09 15:20:59 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   description = "Jaid’s NixOS setup";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    hardware.url = "path:/etc/nixos/hardware-configuration.nix";
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, hardware }: {
     nixosConfigurations.nas = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./src/machines/vm/configuration.nix
+        hardware
       ];
     };
   };
