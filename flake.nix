545d4105d2680081e18d69fde12a28ec2205e60d
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-09 15:03:14 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Jaid’s NixOS setup";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"
+  };
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nas = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./src/machines/vm/configuration.nix
+      ];
+    };
+  };
+}
