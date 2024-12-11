f91cd61f053141bdc656046ee8e9b445755105db
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-11 13:20:45 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/machines/vm/configuration.nix
+++ src/machines/vm/configuration.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
   boot.tmp.cleanOnBoot = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "vm";
