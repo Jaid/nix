5082ba81b29413f7c5d1323bec51a1159cb2ba30
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-11 14:09:52 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/software/docker.nix
+++ src/software/docker.nix
@@ -5,5 +5,20 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
+     # https://gist.github.com/Jaid/3d590adcd183762ad7edffa5eff82c26
+    daemon.settings = {
+      logDriver = "json-file";
+      logOpts = {
+        maxFile = "1";
+        maxSize = "20m";
+      };
+      features.buildkit = true;
+      builder = {
+        gc = {
+          defaultKeepStorage = "4GB";
+          enabled = true;
+        };
+      };
+    };
   };
 }
