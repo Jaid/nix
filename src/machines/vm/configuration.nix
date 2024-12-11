f6a72201d3569d25a38ab37e7bae7f528d583671
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-11 22:45:08 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/machines/vm/configuration.nix
+++ src/machines/vm/configuration.nix
@@ -3,6 +3,9 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
+  virtualisation.vmware.gues = {
+    enable = true;
+  };
   boot.tmp.cleanOnBoot = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "vm";
