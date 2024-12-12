dde1068db4c6227af801d4d7a7aaa67ddb8c4605
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-13 00:50:50 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/homes/jaid.nix
+++ src/homes/jaid.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }: {
   home.packages = [pkgs.httpie];
+  programs.git = {
+    enable = true;
+    userEmail = "joe@example.org";
+    userName = "joe";
+  };
   home.stateVersion = "24.11";
 }
