7662abdcb6d600021310d8132612573312d0de80
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-13 00:59:37 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/homes/jaid.nix
+++ src/homes/jaid.nix
@@ -5,5 +5,13 @@
     userEmail = "joe@example.org";
     userName = "joe";
   };
+  programs.oh-my-posh = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = false;
+    enableNushellIntegration = false;
+    enableZshIntegration = false;
+  };
+  file.".env".text = "FOO=bar";
   home.stateVersion = "24.11";
 }
