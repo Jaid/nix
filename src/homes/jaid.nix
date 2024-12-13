2b09671b7d2e1b7d13cb75fb8db2d6faaf338dcb
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-13 01:49:22 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/homes/jaid.nix
+++ src/homes/jaid.nix
@@ -1,27 +1,29 @@
 { pkgs, ... }: {
-  home = {
-    username = "jaid";
-    homeDirectory = "/home/jaid";
-    packages = with pkgs; [
-      nano
-      gdu
-    ];
-    file.".env".text = "FOO=bar";
-    stateVersion = "24.11";
-  };
-  programs.home-manager = {
-    enable = true;
-  };
-  programs.git = {
-    enable = true;
-    userName = "joe";
-    userEmail = "joe@example.org";
-  };
-  programs.oh-my-posh = {
-    enable = true;
-    enableBashIntegration = true;
-    enableFishIntegration = false;
-    enableNushellIntegration = false;
-    enableZshIntegration = false;
-  };
+  home-manager.users.jaid = {
+    home = {
+      username = "jaid";
+      homeDirectory = "/home/jaid";
+      packages = with pkgs; [
+        nano
+        gdu
+      ];
+      file.".env".text = "FOO=bar";
+      stateVersion = "24.11";
+    };
+    programs.home-manager = {
+      enable = true;
+    };
+    programs.git = {
+      enable = true;
+      userName = "joe";
+      userEmail = "joe@example.org";
+    };
+    programs.oh-my-posh = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = false;
+      enableNushellIntegration = false;
+      enableZshIntegration = false;
+    };
+  }
 }
