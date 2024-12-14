3f2fc03478f8b9777e5563ac21b065cc9bca7d6e
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-14 01:04:52 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/homes/linux/jaid.nix
+++ src/homes/linux/jaid.nix
@@ -11,10 +11,10 @@
         gifski
       ];
       file.".gitmessage".source = ./.resources/.gitmessage;
-      file."config/oh-my-posh/jaid.omp.yml".text = builtins.fetchurl {
+      file."config/oh-my-posh/jaid.omp.yml".text = (builtins.fetchurl {
         url = "https://raw.githubusercontent.com/Jaid/oh-my-posh-config/refs/heads/main/src/jaid.omp.yml";
         sha256 = "02mw9zv7ny0lin4l2v05zmkqbdd7wszdwvc00qaw1hrp53zgxrwk";
-      };
+      });
       stateVersion = "24.11";
     };
     programs.home-manager = {
