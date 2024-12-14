75ac844abd0ffb411285359a3528789f667aec05
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-14 01:41:14 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/homes/linux/.resources/profile.ps1
+++ src/homes/linux/.resources/profile.ps1
@@ -1,1 +1,1 @@
-& "$env:HOME/.local/bin/oh-my-posh" init pwsh --config "$env:reposFolder/oh-my-posh-config/src/jaid.omp.yml" | Invoke-Expression
+& oh-my-posh init pwsh --config ~/.config/oh-my-posh/jaid.omp.yml | Invoke-Expression
