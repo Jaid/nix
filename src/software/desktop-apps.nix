f75ab4f7bcf8a8c4190620655b8e63fdb376461c
Jaid <6216144+Jaid@users.noreply.github.com>
2024-12-15 03:18:58 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/software/desktop-apps.nix
+++ src/software/desktop-apps.nix
@@ -2,14 +2,16 @@
   imports = [
     ./xnview.nix
   ];
-  environment.systemPackages = [
-    pkgs.callPackage ../packages/thorium.nix {}
-    pkgs.vscode
-    pkgs.keeweb
+  environment.systemPackages = let
+    thorium = pkgs.callPackage ../packages/thorium.nix {};
+  in with pkgs; [
+    thorium
+    vscode
+    keeweb
     # pkgs.discord
     # pkgs.ghostty # BLOCKEDBY https://mitchellh.com/writing/ghostty-is-coming - not open-source yet
     # pkgs.chatterino2
-    pkgs.mpv
+    mpv
     # pkgs.krita
     # pkgs.blender
   ];
