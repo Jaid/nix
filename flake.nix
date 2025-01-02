a323e21645507d536a196d5761240520b18c9571
Jaid <6216144+Jaid@users.noreply.github.com>
2025-01-02 03:50:49 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -104,9 +104,28 @@
             (pkgsUnstable.ollama.override {
               acceleration = "cuda";
             })
-            (pkgsLatest.llama-cpp.override {
-              enableCurl = false;
-            })
+            (pkgsLatest.llama-cpp.overrideAttrs (finalAttributes: previousAttributes: {
+              pname = "${previousAttributes.pname}-jaid";
+              meta.description = "${previousAttributes.meta.description} (optimized for AMD Zen 2 and GeForce RTX 40XX)";
+              cmakeFlags =
+                previousAttributes.cmakeFlags
+                ++ [
+                  (pkgs.lib.cmakeBool "GGML_NATIVE" true)
+                  (pkgs.lib.cmakeBool "CMAKE_POSITION_INDEPENDENT_CODE" true)
+                  (pkgs.lib.cmakeFeature "CMAKE_CUDA_FLAGS" "-t6")
+                  (pkgs.lib.cmakeBool "GGML_CUDA_F16" true)
+                  (pkgs.lib.cmakeBool "GGML_CUDA_FA_ALL_QUANTS" true)
+                  (pkgs.lib.cmakeBool "GGML_CUDA_NO_PEER_COPY" true)
+                  (pkgs.lib.cmakeBool "GGML_CUDA" true)
+                  (pkgs.lib.cmakeFeature "GGML_SYCL_TARGET" "NVIDIA")
+                  (pkgs.lib.cmakeFeature "CMAKE_CUDA_ARCHITECTURES" "89")
+                  (pkgs.lib.cmakeFeature "GGML_RPC" "ON")
+                ];
+              env = rec {
+                CFLAGS = "-march=znver2 -mtune=znver2 -O3";
+                CXXFLAGS = CFLAGS;
+              };
+            }))
           ];
         }
         {
