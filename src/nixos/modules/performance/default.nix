2891181b136c5b31e47b1066dedc3b86f28c1b82
Jaid <6216144+Jaid@users.noreply.github.com>
2025-01-17 22:41:09 +0100

    Update
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/nixos/modules/performance/default.nix
+++ src/nixos/modules/performance/default.nix
@@ -12,8 +12,8 @@
   config = pkgs.lib.mkIf (input.config.jaidCustomModules.performance.enable) {
     powerManagement.cpuFreqGovernor = "performance";
     boot.kernelParams = pkgs.lib.mkIf input.config.jaidCustomModules.performance.unhinged ["mitigations=off" "quiet"];
-    vm.swappiness = 1;
     boot.kernel.sysctl = {
+      "vm.swappiness" = 1;
       "net.core.default_qdisc" = "fq";
       "net.ipv4.tcp_congestion_control" = "bbr";
       "kernel.sched_autogroup_enabled" = 0;
