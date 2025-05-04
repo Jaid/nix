06897eefb2085828fc68c8f0f94719e62819105d
Jaid <6216144+Jaid@users.noreply.github.com>
2025-05-04 15:18:11 +0200

    refactor: Removed unused flake inputs and modularized configuration

--- flake.nix
+++ flake.nix
@@ -7,57 +7,58 @@
     home-manager.url = "github:nix-community/home-manager?ref=release-24.11";
   };
   outputs = inputs: let
-    system = "x86_64-linux";
-    cudaComputeCapability = "8.9";
-    cpuArch = "znver2";
-    nixpkgsAttributes = {
-      inherit system;
-      config = {
-        allowUnfree = true;
-        nvidia.acceptLicense = true;
-      };
-    };
-    nixpkgsPersonalAttributes = {
-      inherit system;
-      config = {
-        hostPlatform.gcc.arch = cpuArch;
-        allowUnfree = true;
-        nvidia.acceptLicense = true;
-        cudaSupport = true;
-        cudnnSupport = true;
-        cudaForwardCompat = false;
-        cudaEnableForwardCompat = false;
-        cudaCapabilities = [cudaComputeCapability];
-        rocmSupport = false;
-        packageOverrides = pkgs: {
-          shantell-sans = pkgs.callPackage ./src/nix/packages/shantell-sans.nix {};
-          geologica = pkgs.callPackage ./src/nix/packages/geologica.nix {};
-        };
-      };
-    };
-    pkgs = import inputs.nixpkgs nixpkgsAttributes;
-    pkgsUnstable = import inputs.nixpkgs-unstable nixpkgsAttributes;
-    pkgsLatest = import inputs.nixpkgs-latest nixpkgsAttributes;
-    pkgsPersonal = import inputs.nixpkgs nixpkgsPersonalAttributes;
-    pkgsUnstablePersonal = import inputs.nixpkgs-unstable nixpkgsPersonalAttributes;
-    pkgsLatestPersonal = import inputs.nixpkgs-latest nixpkgsPersonalAttributes;
-    specialArgs = {
-      inherit pkgs;
-      inherit pkgsUnstable;
-      inherit pkgsLatest;
-      inherit pkgsPersonal;
-      inherit pkgsUnstablePersonal;
-      inherit pkgsLatestPersonal;
-    };
     makeMachine = {
       id,
+      system ? "x86_64-linux",
+      cudaComputeCapability ? "8.9",
+      cpuArch ? "znver2",
       modules ? [],
-    }:
+    }: let
+      nixpkgsAttributes = {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          nvidia.acceptLicense = true;
+        };
+      };
+      nixpkgsPersonalAttributes = {
+        inherit system;
+        config = {
+          hostPlatform.gcc.arch = cpuArch;
+          allowUnfree = true;
+          nvidia.acceptLicense = true;
+          cudaSupport = true;
+          cudnnSupport = true;
+          cudaForwardCompat = false;
+          cudaEnableForwardCompat = false;
+          cudaCapabilities = [cudaComputeCapability];
+          rocmSupport = false;
+          packageOverrides = pkgs: {
+            shantell-sans = pkgs.callPackage ./src/nix/packages/shantell-sans.nix {};
+            geologica = pkgs.callPackage ./src/nix/packages/geologica.nix {};
+          };
+        };
+      };
+      pkgs = import inputs.nixpkgs nixpkgsAttributes;
+      pkgsUnstable = import inputs.nixpkgs-unstable nixpkgsAttributes;
+      pkgsLatest = import inputs.nixpkgs-latest nixpkgsAttributes;
+      pkgsPersonal = import inputs.nixpkgs nixpkgsPersonalAttributes;
+      pkgsUnstablePersonal = import inputs.nixpkgs-unstable nixpkgsPersonalAttributes;
+      pkgsLatestPersonal = import inputs.nixpkgs-latest nixpkgsPersonalAttributes;
+      specialArgs = {
+        inherit pkgs;
+        inherit pkgsUnstable;
+        inherit pkgsLatest;
+        inherit pkgsPersonal;
+        inherit pkgsUnstablePersonal;
+        inherit pkgsLatestPersonal;
+      };
+    in
       inputs.nixpkgs.lib.nixosSystem {
         inherit system;
         inherit specialArgs;
         modules =
-         modules
+          modules
           ++ [
             inputs.home-manager.nixosModules.home-manager
             ./src/home-manager/homes/linux/jaid.nix
