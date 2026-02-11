96bae74dd14c6b25db3794e633bf7d9f96d06d50
Jaid <6216144+Jaid@users.noreply.github.com>
2026-02-11 15:19:48 +0000

    Added vps SSH key
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/home-manager/homes/cx/jaid.nix
+++ src/home-manager/homes/cx/jaid.nix
@@ -2,4 +2,5 @@
   imports = [
     ../linux/jaid.nix
   ];
+  home-manager.users.jaid.home.stateVersion = "25.05";
 }
--- src/nixos/machines/cx/configuration.nix
+++ src/nixos/machines/cx/configuration.nix
@@ -7,5 +7,10 @@
     pkgs.alejandra
     pkgs.nixd
   ];
+  users.users.jaid = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCPZ0ZiP841mcBLmSEU4c8zHkkYzpkBUlNPe6vJTCJz vps"
+    ];
+  };
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
