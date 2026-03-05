34421f896505dda659e9ce1ff8fecd1b4957be9c
Jaid <6216144+Jaid@users.noreply.github.com>
2026-03-05 23:42:59 +0000

    Added SSH key

--- src/nixos/machines/hive/configuration.nix
+++ src/nixos/machines/hive/configuration.nix
@@ -36,5 +36,10 @@
       rocmPackages.clr.icd
     ];
   };
+  users.users.jaid = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCde21Ktjoa7PeH3SFH8x59M4s6qxFA6ocp3DxRpnqE localServer"
+    ];
+  };
   users.users.jaid.extraGroups = ["video" "render"];
 }
