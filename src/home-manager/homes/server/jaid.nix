993232a114d0be44708975638979a4fd86d1b3b3
Jaid <6216144+Jaid@users.noreply.github.com>
2026-03-15 01:13:50 +0100

    Switched to Nix syntax for Fastfetch config
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/home-manager/homes/server/jaid.nix
+++ src/home-manager/homes/server/jaid.nix
@@ -9,45 +9,44 @@
       $DRY_RUN_CMD mkdir --parents ${lib.escapeShellArg "${homeDirectory}/docker"}
     '';
     file.".config/fish/functions/fish_greeting.fish".text = "";
-    file.".config/fastfetch/config.jsonc".text = ''
-      {
-        "$schema": "https://raw.githubusercontent.com/fastfetch-cli/fastfetch/master/doc/json_schema.json",
-        "modules": [
-          "title",
-          "separator",
-          "host",
-          "bios",
-          "os",
-          "kernel",
-          "initsystem",
-          "packages",
-          "shell",
-          "cpu",
-          "gpu",
-          "memory",
-          "swap",
-          {
-            "type": "disk",
-            "folders": [
-              "/",
-              "/mnt/data",
-              "/mnt/storage"
-            ],
-            "hideFS": ["autofs"]
-          },
-          "localip",
-          "locale",
-          "break",
-          "colors"
-        ],
-        "display": {
-          "disableLinewrap": true,
-          "pipe": false,
-          "size": {
-            "binaryPrefix": "si"
-          }
-        }
-      }
-    '';
+    home.file.".config/fastfetch/config.jsonc".text = builtins.toJSON {
+      "$schema" = "https://raw.githubusercontent.com/fastfetch-cli/fastfetch/master/doc/json_schema.json";
+      modules = [
+        "title"
+        "separator"
+        "host"
+        "bios"
+        "os"
+        "kernel"
+        "initsystem"
+        "packages"
+        "shell"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        # {
+        #   type = "disk";
+        #   folders = [
+        #     "/"
+        #     "/mnt/data"
+        #     "/mnt/storage"
+        #   ];
+        #   hideFS = ["autofs"];
+        # }
+        "disk"
+        "localip"
+        "locale"
+        "break"
+        "colors"
+      ];
+      display = {
+        disableLinewrap = true;
+        pipe = false;
+        size = {
+          binaryPrefix = "si";
+        };
+      };
+    };
   };
 }
