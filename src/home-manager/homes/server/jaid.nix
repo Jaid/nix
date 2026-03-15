01a3c63f97d7b37b3e664ffbbb79ca6a13ef0f9c
Jaid <6216144+Jaid@users.noreply.github.com>
2026-03-15 01:08:07 +0100

    Added fastfetch config
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/home-manager/homes/server/jaid.nix
+++ src/home-manager/homes/server/jaid.nix
@@ -9,5 +9,45 @@
       $DRY_RUN_CMD mkdir --parents ${lib.escapeShellArg "${homeDirectory}/docker"}
     '';
     file.".config/fish/functions/fish_greeting.fish".text = ""
+    file.".config/fastfetch/config.jsonc".text = ''
+      {
+        "$schema": "https://raw.githubusercontent.com/fastfetch-cli/fastfetch/master/doc/json_schema.json",
+        "modules": [
+          "title",
+          "separator",
+          "host",
+          "bios",
+          "os",
+          "kernel",
+          "initsystem",
+          "packages",
+          "shell",
+          "cpu",
+          "gpu",
+          "memory",
+          "swap",
+          {
+            "type": "disk",
+            "folders": [
+              "/",
+              "/mnt/data",
+              "/mnt/storage"
+            ],
+            "hideFS": ["autofs"]
+          },
+          "localip",
+          "locale",
+          "break",
+          "colors"
+        ],
+        "display": {
+          "disableLinewrap": true,
+          "pipe": false,
+          "size": {
+            "binaryPrefix": "si"
+          }
+        }
+      }
+    '';
   };
 }
